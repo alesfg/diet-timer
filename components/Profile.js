8334430a0fc12c9722002885a05b3601db363f68
kalomano <sowerpablo@gmail.com>
2022-06-28 12:30:50 +0200

    Create Profile.js
    Creating profile component (new window option).

---
+++ components/Profile.js
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import { View, Image, Text, StyleSheet } from "react-native";
+
+function Profile() {
+  return <Text>Holi!</Text>;
+}
+
+export default Profile;
