ca61fe2ce86f40cee5de004cc3c65ee1fbb840e8
alesfg <alex.f.yo@hotmail.com>
2022-07-01 12:13:12 +0200

    uninst datetimepicker

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -2,37 +2,12 @@ import React from 'react'
 import { View, Text, StyleSheet, TouchableOpacity, Platform, Button } from 'react-native'
 import { useState, useEffect } from 'react'
 
-import DateTimePicker from '@react-native-community/datetimepicker';
-
 
 export default function HomeScreen({ name = "Clown" }) {
   const [date, setDate] = useState(new Date())
-  const [mode, setmode] = useState(mode)
-  const [show, setshow] = useState(false)
   const [text, settext] = useState('Empty')
   const [diff, setdiff] = useState(null)
 
-
-  
-  const onChange = (e, selectedDate) => {
-    const currentDate = selectedDate || date
-    setshow(Platform === 'ios')
-    setDate(currentDate)
-
-    let tempDate = new Date(currentDate)
-    let fDate = tempDate.getDate() + (tempDate.getMonth() + 1) + '/' + tempDate.getFullYear()
-    let fTime = 'Hours: ' + tempDate.getHours() + ' | Minutes: ' + tempDate.getMinutes()
-    settext(fDate + '\n' + fTime)
-
-    console.log(fDate)
-    console.log(fTime)
-  }
-
-  const showMode = (currentMode) => {
-    setshow(true)
-    setmode(currentMode)
-  }
-  
 
   return (
     <View style={styles.container}>
@@ -46,18 +21,6 @@ export default function HomeScreen({ name = "Clown" }) {
       {/* Contador */}
       <View>
       <Text>{text}</Text>
-      <Button title='Fecha' onPress={() => showMode('date')} />
-      <Button title='Hora' onPress={() => showMode('time')} />
-      {show && (
-        <DateTimePicker
-        testID = 'dateTimePicker'
-        value={date}
-        mode={mode}
-        is24Hour={true}
-        display='default'
-        onChange={onChange}
-        />
-      )}
         <TouchableOpacity>
           <Text>He pecado 🍖</Text>
         </TouchableOpacity>
