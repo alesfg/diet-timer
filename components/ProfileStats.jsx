ee4c16cb2658fb15c4d06c5234e25b936f0a3548
kalomano <sowerpablo@gmail.com>
2022-07-07 14:02:10 +0200

    ScrollView a FlatList (sucio...)
    cambio de ScrollVIew a flatList para añadir animacion

--- components/ProfileStats.jsx
+++ components/ProfileStats.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useRef } from "react";
 import {
   View,
   Image,
@@ -7,40 +7,112 @@ import {
   TouchableOpacity,
   Button,
 } from "react-native";
-import { ScrollView } from "react-native-gesture-handler";
+import { FlatList, ScrollView } from "react-native-gesture-handler";
 import DateIcon from "../assets/ProfileStats/DateIcon.png";
 
 const ProfileStats = () => {
   const [statsData, setStatsData] = useState(getProfileData());
+  const [alturaAux, setAlturaAux] = useState(140);
+  const ref1 = [
+    useRef(),
+    useRef(),
+    useRef(),
+    useRef(),
+    useRef(),
+    useRef(),
+    useRef(),
+    useRef(),
+    useRef(),
+    useRef(),
+    useRef(),
+    useRef(),
+    useRef(),
+    useRef(),
+    useRef(),
+    useRef(),
+  ];
 
   return (
-    <ScrollView
+    // <ScrollView
+    //   onScroll={(eventAux) => handleScroll(ref1, setAlturaAux, eventAux)}
+    //   showsVerticalScrollIndicator={false}
+    //   showsHorizontalScrollIndicator={false}
+    //   style={{
+    //     marginTop: 0,
+    //     width: 400,
+    //   }}
+    //   contentContainerStyle={{
+    //     justifyContent: "center",
+    //     flexDirection: "row",
+    //     flexWrap: "wrap",
+    //     padding: 25,
+    //     paddingTop: 5,
+    //   }}
+    // >
+    //   {statsData.map((stats, i) => (
+    // <View
+    //   key={stats.title}
+    //   ref={ref1[i]}
+    //   style={{
+    //     backgroundColor: "rgba(48, 88, 230, 0.55)",
+    //     borderRadius: 40,
+    //     height: alturaAux,
+    //     width: alturaAux,
+    //     alignItems: "center",
+    //     margin: 10,
+    //     padding: 20,
+    //     justifyContent: "center",
+    //     shadowColor: "#000",
+    //     shadowOffset: { width: 0, height: 10 },
+    //     shadowOpacity: 0.3,
+    //     shadowRadius: 20,
+    //   }}
+    // >
+    //   <View
+    //     style={{
+    //       width: 50,
+    //       height: 50,
+    //       borderColor: "#727272",
+    //       borderWidth: 1,
+    //       borderRadius: 25,
+    //       justifyContent: "center",
+    //       alignItems: "center",
+    //     }}
+    //   >
+    //     <Image
+    //       source={DateIcon}
+    //       style={{ width: 30, height: 30, tintColor: "#DC1D1D" }}
+    //     ></Image>
+    //   </View>
+    //   <Text style={{ color: "black", textAlign: "center" }}>
+    //     {stats.title}
+    //   </Text>
+    //   <Text style={{ color: "white", textAlign: "center" }}>
+    //     {stats.description}
+    //   </Text>
+    // </View>
+    //   ))}
+    // </ScrollView>
+    <FlatList
       showsVerticalScrollIndicator={false}
-      showsHorizontalScrollIndicator={false}
-      style={{
-        marginTop: 0,
-        width: 400,
-      }}
-      contentContainerStyle={{
-        justifyContent: "center",
-        flexDirection: "row",
-        flexWrap: "wrap",
-        padding: 25,
-        paddingTop: 5,
-      }}
-    >
-      {statsData.map((stats) => (
+      numColumns={2}
+      data={statsData}
+      renderItem={({ item: stats }) => (
         <View
           key={stats.title}
           style={{
             backgroundColor: "rgba(48, 88, 230, 0.55)",
             borderRadius: 40,
-            height: 130,
-            width: 130,
+            height: alturaAux,
+            width: alturaAux,
             alignItems: "center",
             margin: 10,
             padding: 20,
             justifyContent: "center",
+            shadowColor: "#000",
+            shadowOffset: { width: 0, height: 10 },
+            shadowOpacity: 0.3,
+            shadowRadius: 20,
           }}
         >
           <View
@@ -66,9 +138,18 @@ const ProfileStats = () => {
             {stats.description}
           </Text>
         </View>
-      ))}
-    </ScrollView>
+      )}
+    />
   );
+};
+
+const handleScroll = (referencia, setAltura, event) => {
+  // let alturaRelativa = event.nativeEvent. -event.nativeEvent.contentOffset.y;
+  //console.log("content: " + event.nativeEvent.contentOffset.y);
+  console.log("AlturaPrimerElemento: " + 1);
+
+  //setAltura(100);
+  console.log(referencia[0].current.clientHeight);
 };
 
 const getProfileData = () => {
