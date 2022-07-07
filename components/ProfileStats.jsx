195f95bb30ef509c715a18e1e38db555c4194309
kalomano <sowerpablo@gmail.com>
2022-07-07 11:51:51 +0200

    Basic Profile Style
    Añado un estilo básico a la página de perfil

--- components/ProfileStats.jsx
+++ components/ProfileStats.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 import {
   View,
   Image,
@@ -7,26 +7,67 @@ import {
   TouchableOpacity,
   Button,
 } from "react-native";
-import { FlatList } from "react-native-gesture-handler";
+import { ScrollView } from "react-native-gesture-handler";
+import DateIcon from "../assets/ProfileStats/DateIcon.png";
 
 const ProfileStats = () => {
   const [statsData, setStatsData] = useState(getProfileData());
 
   return (
-    <FlatList
-      style={{ margin: 40, marginTop: 0 }}
-      // style={{ marginBottom: 100 }}
-      data={statsData}
-      renderItem={({ item: stats }) => (
-        <View>
-          <Text style={{ fontWeight: "bold" }}>{stats.title}</Text>
-          <Text>{stats.description}</Text>
+    <ScrollView
+      showsVerticalScrollIndicator={false}
+      showsHorizontalScrollIndicator={false}
+      style={{
+        marginTop: 0,
+        width: 400,
+      }}
+      contentContainerStyle={{
+        justifyContent: "center",
+        flexDirection: "row",
+        flexWrap: "wrap",
+        padding: 25,
+        paddingTop: 5,
+      }}
+    >
+      {statsData.map((stats) => (
+        <View
+          key={stats.title}
+          style={{
+            backgroundColor: "rgba(48, 88, 230, 0.55)",
+            borderRadius: 40,
+            height: 130,
+            width: 130,
+            alignItems: "center",
+            margin: 10,
+            padding: 20,
+            justifyContent: "center",
+          }}
+        >
+          <View
+            style={{
+              width: 50,
+              height: 50,
+              borderColor: "#727272",
+              borderWidth: 1,
+              borderRadius: 25,
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Image
+              source={DateIcon}
+              style={{ width: 30, height: 30, tintColor: "#DC1D1D" }}
+            ></Image>
+          </View>
+          <Text style={{ color: "black", textAlign: "center" }}>
+            {stats.title}
+          </Text>
+          <Text style={{ color: "white", textAlign: "center" }}>
+            {stats.description}
+          </Text>
         </View>
-      )}
-      ItemSeparatorComponent={() => (
-        <Text>-------------------------------</Text>
-      )}
-    />
+      ))}
+    </ScrollView>
   );
 };
 
@@ -35,34 +76,34 @@ const getProfileData = () => {
 
   data[0] = {
     title: "Last diet date:",
-    description: "Pues el último día que comenzaste la dieta",
+    description: "HOY",
   };
   data[1] = {
     title: "Longest diet:",
-    description: "Pues has durado un montón de tiempo",
+    description: "3 Days!",
   };
   data[2] = {
     title: "Times diet had been broken:",
-    description: "has roto la dieta mazo veces hermano",
+    description: "4",
   };
   data[3] = {
     title: "Maximum badge level:",
-    description: "Alcanzaste la badge de puto amo",
+    description: "PEPE",
   };
   data[4] = {
     title: "First diet date:",
-    description: "la primera vez que usaste la aplicacion pues era hace mazo",
+    description: "12/03/2002",
   };
   data[5] = {
-    title: "asdasd",
+    title: "asda123sd",
     description: "asdasd",
   };
   data[6] = {
-    title: "asdasd:",
+    title: "asd232asd:",
     description: "asdasd",
   };
   data[7] = {
-    title: "asdasd",
+    title: "asda43sd",
     description: "asdasd",
   };
 
