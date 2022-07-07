66b2076d5eb7d8c4bce7e7c68529e8a188408d46
kalomano <sowerpablo@gmail.com>
2022-07-07 14:50:39 +0200

    Animacion añadida Profile (sucio)
    Esta sucio, falta por arreglar de todo, pero he añadido una animacion que hace que desaparezcan hacia arriba las cosas estas
    https://www.youtube.com/watch?v=F8x-dyIsrJ8

--- components/ProfileStats.jsx
+++ components/ProfileStats.jsx
@@ -8,6 +8,7 @@ import {
   Button,
 } from "react-native";
 import { FlatList, ScrollView } from "react-native-gesture-handler";
+import Animated from "react-native-reanimated";
 import DateIcon from "../assets/ProfileStats/DateIcon.png";
 
 const ProfileStats = () => {
@@ -31,7 +32,7 @@ const ProfileStats = () => {
     useRef(),
     useRef(),
   ];
-
+  const scrollY = React.useRef(new Animated.Value(0)).current;
   return (
     // <ScrollView
     //   onScroll={(eventAux) => handleScroll(ref1, setAlturaAux, eventAux)}
@@ -93,52 +94,81 @@ const ProfileStats = () => {
     // </View>
     //   ))}
     // </ScrollView>
-    <FlatList
+    <Animated.FlatList
+      contentContainerStyle={{ paddingBottom: 22 }}
+      onScroll={Animated.event(
+        [{ nativeEvent: { contentOffset: { y: scrollY } } }],
+        { useNativeDriver: true }
+      )}
       showsVerticalScrollIndicator={false}
       numColumns={2}
       data={statsData}
-      renderItem={({ item: stats }) => (
-        <View
-          key={stats.title}
-          style={{
-            backgroundColor: "rgba(48, 88, 230, 0.55)",
-            borderRadius: 40,
-            height: alturaAux,
-            width: alturaAux,
-            alignItems: "center",
-            margin: 10,
-            padding: 20,
-            justifyContent: "center",
-            shadowColor: "#000",
-            shadowOffset: { width: 0, height: 10 },
-            shadowOpacity: 0.3,
-            shadowRadius: 20,
-          }}
-        >
-          <View
+      renderItem={({ item: stats, index }) => {
+        const inputRange = [
+          -1,
+          0,
+          (index % 2 === 0 ? index : index - 1) * alturaAux * 0.5,
+          alturaAux * 0.5 * ((index % 2 === 0 ? index : index - 1) + 1.5),
+        ];
+        const opinputRange = [
+          -1,
+          0,
+          (index % 2 === 0 ? index : index - 1) * alturaAux,
+          alturaAux * ((index % 2 === 0 ? index : index - 1) + 0.1),
+        ];
+        const scale = scrollY.interpolate({
+          inputRange,
+          outputRange: [1, 1, 1, 0],
+        });
+        const opacity = scrollY.interpolate({
+          inputRange,
+          outputRange: [1, 1, 1, 0],
+        });
+        return (
+          <Animated.View
+            key={stats.title}
             style={{
-              width: 50,
-              height: 50,
-              borderColor: "#727272",
-              borderWidth: 1,
-              borderRadius: 25,
-              justifyContent: "center",
+              backgroundColor: "rgba(48, 88, 230, 0.55)",
+              borderRadius: 40,
+              height: alturaAux,
+              width: alturaAux,
               alignItems: "center",
+              margin: 10,
+              padding: 20,
+              justifyContent: "center",
+              shadowColor: "#000",
+              shadowOffset: { width: 0, height: 10 },
+              shadowOpacity: 0.3,
+              shadowRadius: 20,
+              transform: [{ scale }],
+              opacity,
             }}
           >
-            <Image
-              source={DateIcon}
-              style={{ width: 30, height: 30, tintColor: "#DC1D1D" }}
-            ></Image>
-          </View>
-          <Text style={{ color: "black", textAlign: "center" }}>
-            {stats.title}
-          </Text>
-          <Text style={{ color: "white", textAlign: "center" }}>
-            {stats.description}
-          </Text>
-        </View>
-      )}
+            <View
+              style={{
+                width: 50,
+                height: 50,
+                borderColor: "#727272",
+                borderWidth: 1,
+                borderRadius: 25,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Image
+                source={DateIcon}
+                style={{ width: 30, height: 30, tintColor: "#DC1D1D" }}
+              ></Image>
+            </View>
+            <Text style={{ color: "black", textAlign: "center" }}>
+              {stats.title}
+            </Text>
+            <Text style={{ color: "white", textAlign: "center" }}>
+              {stats.description}
+            </Text>
+          </Animated.View>
+        );
+      }}
     />
   );
 };
