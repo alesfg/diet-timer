0dbbde1f104117d9c842db822669f27bdeeaad49
Danielin <danielxroh@gmail.com>
2022-07-08 17:07:34 +0200

    Revert "Merge branch 'master' of https://github.com/alesfg/diet-timer"
    This reverts commit 52efecf60e6a68b45d7165840b194097fcf4ac91, reversing
    changes made to 16e15edadf3d058c72e0474813f1bfb8dc95166e.

--- components/Badges.js
+++ components/Badges.js
@@ -1,29 +1,32 @@
-import React from "react";
-import { View, StyleSheet } from "react-native";
-import { useState } from "react";
-import Badge from "./Badge";
-import badges from "./badgesArray.js";
+import React from 'react'
+import { View, Image, Text, StyleSheet } from 'react-native'
+import { useState } from 'react'
+import clown from '../assets/clown.jpg'
+import noob from '../assets/noob.png'
+import average from '../assets/average.jpg'
+import fitness from '../assets/fitness.png'
+import Badge from './Badge'
 
 export default function Badges() {
+    const badges = [
+        {name: "Clown", img: clown, time: "0+ Hours",id:1},
+        {name: "Noob", img: noob, time: "24+ Hours",id:2},
+        {name: "Average", img: average, time: "48+ Hours",id:3},
+        {name: "Fitness", img: fitness, time: "100+ Hours",id:4}
+    ]
+    const [badge, setBadge] = useState(badges[0])
   return (
     <View style={styles.container}>
-      {badges.map((badge) => (
-        <Badge
-          name={badge.name}
-          img={badge.img}
-          text={badge.text}
-          key={badge.id}
-        />
-      ))}
+        {badges.map(badge => <Badge name={badge.name} img={badge.img} time={badge.time} key={badge.id} />)}
     </View>
-  );
+  )
 }
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    height: "100%",
-    backgroundColor: "#eaeaa0",
-    color: "#8ab971",
-    padding: 100,
-  },
-});
+    container: {
+        flex: 1,
+        height: '100%',
+        backgroundColor: '#eaeaa0',
+        color: '#8ab971',
+        padding:100
+    }
+})
--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,10 +1,9 @@
 import React from "react";
-import { View, Image, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import { useState, useEffect } from "react";
 import Timer from "./Timer";
 import styles from "./Styles";
 import storage from "../storage/storage";
-import badges from "./badgesArray.js";
 
 export default function HomeScreen({ name = "Clown" }) {
   const [initialDate, setInitialDate] = useState(null);
@@ -21,14 +20,10 @@ export default function HomeScreen({ name = "Clown" }) {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.tittle}>DIET TIMER</Text>
-      <View style={styles.badge}>
-        <Text style={styles.badgeText}>Current Badge:</Text>
-        <Image
-          source={badges[calcularBadge()].img}
-          resizeMode="cover"
-          style={styles.badgeImage}
-        />
+      <View>
+        <Text style={styles.tittle}>DIET TIMER</Text>
+        <Text>{name}</Text>
+        <Text>Current Badge</Text>
       </View>
       <View style={styles.containerTimerButton}>
         <Timer initialDate={initialDate} />
@@ -43,21 +38,6 @@ export default function HomeScreen({ name = "Clown" }) {
     </View>
   );
 
-  function calcularBadge() {
-    let diff = new Date() - initialDate;
-    let badgeActual = 1;
-
-    //recorremos cada badge, y la primera que encontramos con un valor tiempo mayor al que tenemos nos salimos
-    for (let i = 0; i < badges.length; i++) {
-      if (badges[i].time > diff) {
-        badgeActual = i;
-        break;
-      } else if (i == badges.length - 1) {
-        badgeActual = i + 1;
-      }
-    }
-    return badgeActual - 1;
-  }
   function reset() {
     const date = new Date();
     setInitialDate(date);
